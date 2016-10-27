087f988b2731689fc6a556feabbe14a20b461ad0
Andrew Crilly <Andrew.Crilly@triad.co.uk>
2016-10-27 09:53:31 +0000

    mailing list

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,30 @@
 class VisitorsController < ApplicationController
 
   def new
-    @owner = Owner.new
+    # Assign the Visitor model to an instance variable
+    @visitor = Visitor.new
+  end
+
+  def create
+    # Instantiate the visitor instance variable with attributes from the submitted form
+    @visitor = Visitor.new(secure_params)
+
+    # Validate the visitor instance variable
+    if @visitor.valid?
+      # If valid, subcribe the visitor to the mailing list and redirect to the home page
+      @visitor.subscribe
+      flash[:notice] = "Signed up #{@visitor.email}."
+      redirect_to root_path
+    else
+      # If invalid, redisplay the home page (the new action)
+      render :new
+    end
+  end
+
+  private
+
+  def secure_params
+    params.require(:visitor).permit(:email)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Use resourceful route for Contact and Visitor controllers
   resources :contacts, only: [:new, :create]
-  root to: 'visitors#new'
+  resources :visitors, only: [:new, :create]
+  
+   # The application root (the home page) is the vistors#new view
+   root to: 'visitors#new'
 end
